999cbf83d0816139eb6e90b54588a1c26c6d3fde
Alice <algus_91@mail.rugit config --global user.email algus_91@mail.ru>
2022-08-16 19:54:42 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[3];
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i >= 0; i--)
+{
+    array[i] = number % 10;
+    number /= 10;
+}
+Console.WriteLine(array[array.Length - 2]);
+
