371e04f8c8908f8b30a195daa61fadb33e25bc8c
Alice <algus_91@mail.rugit config --global user.email algus_91@mail.ru>
2022-08-16 19:56:41 +0300

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число обозначающее день недели");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+if (weekday == 1)
+{
+    Console.WriteLine("нет");
+}
+else if (weekday == 2) 
+{
+    Console.WriteLine("нет");
+}
+else if (weekday == 3)
+{
+    Console.WriteLine("нет");
+}
+else if (weekday == 4)
+{
+    Console.WriteLine("нет");
+}
+else if (weekday == 5)
+{
+    Console.WriteLine("нет");
+}
+else if (weekday == 6)
+{
+    Console.WriteLine("да");
+}
+else if (weekday == 7)
+{
+    Console.WriteLine("да");
+}
