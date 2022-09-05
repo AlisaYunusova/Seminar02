45b56ade3ae23eebd0009ac1066da55953745ac8
AlisaYunusova <110364570+AlisaYunusova@users.noreply.github.com>
2022-09-05 18:14:24 +0300

    Newtext

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] array = new int[3];
+// Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int[] array = new int[3];
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -9,3 +10,23 @@ for (int i = 2; i >= 0; i--)
 }
 Console.WriteLine(array[array.Length - 2]);
 
+//Через массив не соответсвует ТЗ. Потому что надо работать с числом. Правильно:
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int lenghtNumber = 0;
+int copyNumber = number;
+
+while (copyNumber > 0)
+{
+    copyNumber /= 10;
+    lenghtNumber ++;
+}
+Console.WriteLine(lenghtNumber);
+
+int power = 1;
+while (lenghtNumber>3)
+{
+    power *=10;
+    lenghtNumber --;
+}
+Console.WriteLine(number/power % 10);
